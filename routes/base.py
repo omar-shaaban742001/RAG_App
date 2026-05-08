8cbf3afad99383a427139b288f4306f6fcf83d9b
omar-shaaban742001 <omarshaaban742001@gmail.com>
2026-05-08 17:31:09 +0300

    separete routes in diffrent dir

---
+++ routes/base.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, APIRouter
+import os
+
+base_route = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"],
+)
+
+
+@base_route.get("/")
+async def welcome():
+
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+
+    return {
+        "App Name: {app_name}",
+        "App Verison: {app_version}"
+    }
